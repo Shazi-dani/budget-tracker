50a5d8d671beee8a1b62cd4602e3a77432129757
shazina danish <shazinamunir26@gmail.com>
2023-07-18 00:32:31 +0000

    Create Refrences Variables

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//Create Refrences Variables
+let totalAmount = document.getElementById("total-amount");
+let expenseAmount = document.getElementById("expense-amount");
+let totalAmountButton = document.getElementById("total-amount-button");
+let checkAmountButton = document.getElementById("check-amount");
+let errorMessage = document.getElementById("budget-error");
+let expenseError = document.getElementById("expense-error");
+let budgetDate = document.getElementById("budget-date");
+let expenseDate = document.getElementById("expense-date");
+let expenseTitle = document.getElementById("expense-title");
+let amount = document.getElementById("amount");
+let expenditureValue = document.getElementById("expenditure-value");
+let list = document.getElementById("list");
+let tempAmount = 0;
